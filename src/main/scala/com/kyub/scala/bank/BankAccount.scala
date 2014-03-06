dc426645123e7b0d9b3f042cd0719f29f434f779
sammyrulez <sammyrulez@gmail.com>
2014-03-06 09:05:31 +0100

    more functional case usage

--- src/main/scala/com/kyub/scala/bank/BankAccount.scala
+++ src/main/scala/com/kyub/scala/bank/BankAccount.scala
@@ -1,18 +1,19 @@
 package com.kyub.scala.bank
 
 class BankAccount(var balance: Double = 0) {
-  
-  def deposit(money:Double){
-    if(money > 0.)
-    	balance = balance + money
+
+  def deposit(money: Double) {
+    if (money > 0.)
+      balance = balance + money
   }
-  
-  def withdraw(money:Double){
-    if(money > 0.)
-    	if(money > balance)
-    		balance = 0.
-    	else
-    		balance = balance - money
+
+  def withdraw(money: Double) {
+    money match {
+      case x if x < 0 => println("Failing silenty")
+      case x if x > balance => balance = 0.
+      case otherNumber => balance = balance - money
+    }
+
   }
 
 }
